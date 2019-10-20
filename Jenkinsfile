f95dcee62e9daa74495a67f3b3914f4204fef561
Soumya Mukherjee <mukherjee.soumya@gmail.com>
2019-10-20 22:43:32 +0530

    Updated Docker-Compose

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,7 +7,7 @@ pipeline {
                 sh 'docker rm $(docker ps -a -q)'
             }
         }
-        stage('Preparation') { 
+        stage('Pull latest Code') { 
              steps {
               // Get some code from a GitHub repository
               git 'https://github.com/pandasm/pipelinetest.git'
@@ -27,6 +27,12 @@ pipeline {
         stage('Test') {
             steps {
                 sh 'mvn test'
+            }
+        }
+        stage('Destroy - Any Running Containers') { 
+            steps {
+                sh 'docker stop $(docker ps -a -q)'
+                sh 'docker rm $(docker ps -a -q)'
             }
         }
     }
--- src/test/java/sampletest/GoogleTest.java
+++ src/test/java/sampletest/GoogleTest.java
@@ -26,18 +26,24 @@ public class GoogleTest {
         options.addArguments("headless");
         
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        System.out.println("Initialised the Deiver and sent the script to grid");
         
     }
 
     @Test
     public void googleTest() throws InterruptedException {
     	driver.get("https://www.google.com");
+    	System.out.println("Opened the google application");
         driver.findElement(By.name("q")).sendKeys("Automation");
+        System.out.println("Searching for work Automation");
         driver.findElement(By.name("btnK")).click();
+        System.out.println("Clicked on the button");
         
         int size = driver.findElements(By.className("rc")).size();
+        System.out.println("Retrieving the number of links");
         
         Assert.assertTrue(size >= 10);
+        System.out.println("Asseted Successfull");
     }
     
     @AfterTest
